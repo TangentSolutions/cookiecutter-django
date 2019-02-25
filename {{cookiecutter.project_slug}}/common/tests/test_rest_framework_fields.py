e884df0f6ef321a6f28d35a99205fed95053eb73
Bradley Stuart Kirton <bradleykirton@gmail.com>
2019-02-25 14:30:43 +0200

    Updated tests in common package

--- {{cookiecutter.project_slug}}/common/tests/test_rest_framework_fields.py
+++ {{cookiecutter.project_slug}}/common/tests/test_rest_framework_fields.py
@@ -4,34 +4,36 @@ from common.rest_framework.fields import LuhnField, CompanyRegistrationNumberFie
 import pytest
 
 
-def test_rest_framework_luhn_field_raises_validation_error():
-    """Ensure that the rest framework field raises a validation error
-    for an invalid input."""
+class TestLuhnField:
+    def test_rest_framework_luhn_field_raises_validation_error(self):
+        """Ensure that the rest framework field raises a validation error
+        for an invalid input.
+        """
 
-    with pytest.raises(ValidationError):
-        field = LuhnField()
-        field.to_internal_value('testing')
+        with pytest.raises(ValidationError):
+            field = LuhnField()
+            field.to_internal_value("testing")
 
+    def test_rest_framework_luhn_field(self):
+        """Ensure that the rest framework field returns the value when it is valid."""
 
-def test_rest_framework_luhn_field():
-    """Ensure that the rest framework field returns the value when it is valid."""
-
-    assert LuhnField().to_internal_value('1928376459096') == '1928376459096'
-
-
-def test_rest_framework_company_registration_number_field_raises_validation_error():
-    """Ensure that the rest framework field raises a validation error
-    for an invalid input."""
-
-    with pytest.raises(ValidationError):
-        field = CompanyRegistrationNumberField()
-        field.to_internal_value('testing')
+        assert LuhnField().to_internal_value("1928376459096") == "1928376459096"
 
 
-def test_rest_framework_company_registration_number_field():
-    """Ensure that the rest framework field returns the value when it is valid."""
+class TestCompanyRegistrationNumberField:
+    def test_raises_validation_error(self):
+        """Ensure that the rest framework field raises a validation error
+        for an invalid input.
+        """
 
-    assert (
-        CompanyRegistrationNumberField().to_internal_value('2018/029674/07')
-        == '2018/029674/07'
-    )
+        with pytest.raises(ValidationError):
+            field = CompanyRegistrationNumberField()
+            field.to_internal_value("testing")
+
+    def test_rest_framework_company_registration_number_field(self):
+        """Ensure that the rest framework field returns the value when it is valid."""
+
+        assert (
+            CompanyRegistrationNumberField().to_internal_value("2018/029674/07")
+            == "2018/029674/07"
+        )
--- {{cookiecutter.project_slug}}/common/tests/test_validation.py
+++ {{cookiecutter.project_slug}}/common/tests/test_validation.py
@@ -7,54 +7,57 @@ from common.validation import (
 import pytest
 
 
-def test_is_valid_luhn_algorithm():
-    """Test that a given string is valid according to luhn's algorithm."""
+class TestIsValidLuhnAlgorithm:
+    def test_is_valid_luhn_algorithm(self):
+        """Test that a given string is valid according to luhn's algorithm."""
 
-    assert is_valid_luhn_algorithm("1928376459096") is True
+        assert is_valid_luhn_algorithm("1928376459096") is True
 
+    def test_is_valid_luhn_algorithm_raises_type_error(self):
+        """Ensure that if given a value not of type string a type error is raised."""
 
-def test_is_valid_luhn_algorithm_raises_type_error():
-    """Ensure that if given a value not of type string a type error is raised."""
+        with pytest.raises(TypeError):
+            is_valid_luhn_algorithm(100)
 
-    with pytest.raises(TypeError):
-        is_valid_luhn_algorithm(100)
+    def test_is_valid_luhn_algorithm_raises_value_error(self):
+        """Ensure that if given a string that contains non digits a value error is raised."""
 
-
-def test_is_valid_luhn_algorithm_raises_value_error():
-    """Ensure that if given a string that contains non digits a value error is raised."""
-
-    with pytest.raises(ValueError):
-        is_valid_luhn_algorithm("100testing")
-
-
-def test_append_check_digit_luhn_algorithm():
-    """Test that the generated check digit is valid."""
-
-    assert append_check_digit_luhn_algorithm("192837645909")[-1] == "6"
+        with pytest.raises(ValueError):
+            is_valid_luhn_algorithm("100testing")
 
 
-def test_append_check_digit_luhn_algorithm_raises_type_error():
-    """Ensure that if given a value not of type string a type error is raised."""
+class TestAppendCheckDigitLuhnAlgorithm:
+    def test_append_check_digit_luhn_algorithm(self):
+        """Test that the generated check digit is valid."""
 
-    with pytest.raises(TypeError):
-        append_check_digit_luhn_algorithm(100)
+        assert append_check_digit_luhn_algorithm("192837645909")[-1] == "6"
+
+    def test_append_check_digit_luhn_algorithm_raises_type_error(self):
+        """Ensure that if given a value not of type string a type error is raised."""
+
+        with pytest.raises(TypeError):
+            append_check_digit_luhn_algorithm(100)
+
+    def test_append_check_digit_luhn_algorithm_raises_value_error(self):
+        """Ensure that if given a string that contains non digits a value error is raised."""
+
+        with pytest.raises(ValueError):
+            append_check_digit_luhn_algorithm("100testing")
 
 
-def test_append_check_digit_luhn_algorithm_raises_value_error():
-    """Ensure that if given a string that contains non digits a value error is raised."""
+class TestIsValidCipcRegistrationNumber:
+    def test_is_valid_cipc_registration_number(self):
+        """Test the cipc company registration number validator."""
 
-    with pytest.raises(ValueError):
-        append_check_digit_luhn_algorithm("100testing")
+        assert is_valid_cipc_registration_number("2018/209387/07") is True
 
+    def test_is_valid_cipc_registration_number_raises_type_error(self):
+        """Ensure that if given a value not of type string a type error is raised."""
 
-def test_is_valid_cipc_registration_number():
-    """Test the cipc company registration number validator."""
+        with pytest.raises(TypeError):
+            is_valid_cipc_registration_number(100)
 
-    assert is_valid_cipc_registration_number("2018/209387/07") is True
+    def test_invalid_format(self):
+        """Ensure false is returned if an invalid format is provided."""
 
-
-def test_is_valid_cipc_registration_number_raises_type_error():
-    """Ensure that if given a value not of type string a type error is raised."""
-
-    with pytest.raises(TypeError):
-        is_valid_cipc_registration_number(100)
+        assert is_valid_cipc_registration_number("testingtesting") is False
